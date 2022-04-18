5652819ceb85575956b24876d20e953f468794c3
Kuznetsov. S. I <kuznetsov.s.i@bk.ru>
2022-04-18 09:24:13 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Sergey");
+        System.out.println("04.05.1995");
+
+    }
+}
