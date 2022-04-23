999b4cc5fe70414555d571edb8fa3adcdc443ffa
Kuznetsov S. I <kuznetsov.s.i@bk.ru>
2022-04-23 12:52:30 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -3,12 +3,24 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
-        System.out.println("Array size: " + ages.length);
+        System.out.println("Array size: " + ages.length
+                + System.lineSeparator());
 
         String[] surnames = new String[100500];
-        System.out.println("Array size: " + surnames.length);
+        System.out.println("Array size: " + surnames.length
+                + System.lineSeparator());
 
         float[] prises = new float[40];
-        System.out.println("Array size: " + prises.length);
+        System.out.println("Array size: " + prises.length
+                + System.lineSeparator());
+
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Sergey Kuznetsov";
+        names[2] = "Nikola Tesla";
+        names[3] = "Hayao Miadzaki";
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
