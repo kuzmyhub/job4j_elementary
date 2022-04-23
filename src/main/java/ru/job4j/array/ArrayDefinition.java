60d260c8f39fe62bb281c7adada1c783a7c9e831
Kuznetsov S. I <kuznetsov.s.i@bk.ru>
2022-04-23 12:41:20 +0300

    6.0.1.1. Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -3,7 +3,12 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
+        System.out.println("Array size: " + ages.length);
+
         String[] surnames = new String[100500];
+        System.out.println("Array size: " + surnames.length);
+
         float[] prises = new float[40];
+        System.out.println("Array size: " + prises.length);
     }
 }
