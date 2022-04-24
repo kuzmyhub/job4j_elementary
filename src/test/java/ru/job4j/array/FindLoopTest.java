61a04a232c01ea9063f6b7a31cd9e96d1d736204
Kuznetsov S. I <kuznetsov.s.i@bk.ru>
2022-04-24 15:21:40 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -24,4 +24,37 @@ public class FindLoopTest {
         int expected = -1;
         Assert.assertEquals(expected, result);
     }
+
+    @Test
+    public void whenDiapasonHas2Then3() {
+        int[] data = {5, 2, 10, 2, 4};
+        int el = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 3;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenDiapasonHas6Then6() {
+        int[] data = {3, 5, 7, 9, 11, 13, 15};
+        int el = 15;
+        int start = 0;
+        int finish = 6;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 6;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenDiapasonHasXThenX() {
+        int[] data = {2, 3, 4, 5, 6, 7};
+        int el = 2;
+        int start = 0;
+        int finish = 3;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 0;
+        Assert.assertEquals(expected, result);
+    }
 }
