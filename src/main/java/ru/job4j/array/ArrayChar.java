391e5b49806fb73bc8685c7c55cc0f42d5e21c39
Kuznetsov S. I <kuznetsov.s.i@bk.ru>
2022-04-24 11:12:56 +0300

    6.4. Слово начинается с ... [#41585]

---
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = true;
+        int arrayLength = Math.min(word.length, pref.length);
+        for (int i = 0; i < arrayLength; i++) {
+            String wordString = Character.toString(word[i]);
+            String prefString = Character.toString(pref[i]);
+            if (wordString.startsWith(prefString) == false) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
