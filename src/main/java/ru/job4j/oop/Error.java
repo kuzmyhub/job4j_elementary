ffbf268be270e4431cb02394abb453b55d6f7526
Kuznetsov S. I <kuznetsov.s.i@bk.ru>
2022-04-28 20:46:53 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,36 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Error def = new Error();
+        Error first = new Error(true, 201, "Created");
+        Error second = new Error(true, 401, "Unauthorized");
+        Error third = new Error(true, 507, "Insufficient Storage");
+
+        def.printInfo();
+        first.printInfo();
+        second.printInfo();
+        third.printInfo();
+    }
+}
