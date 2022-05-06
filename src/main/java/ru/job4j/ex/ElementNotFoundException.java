1b56031cf0699ec663e8f3dd44d18c87b2fbf284
Kuznetsov. S. I <kuznetsov.s.i@bk.ru>
2022-05-06 08:12:15 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/ElementNotFoundException.java
@@ -0,0 +1,9 @@
+package ru.job4j.ex;
+
+import java.lang.Exception;
+
+public class ElementNotFoundException extends Exception {
+    public ElementNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,25 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException(
+                    "The element is missing in the array");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            indexOf(new String[]{"a", "to", "the", "at"}, "in");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
